7b8338e498bd10fce7c1667cc0a8f2dbc0f37db7
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-04-17 09:03:53 -0700

    navbar styling and mobile reponsiveness

--- src/components/navbar/Navbar.component.tsx
+++ src/components/navbar/Navbar.component.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { Menu, LightMode, DarkMode } from "@mui/icons-material";
@@ -13,14 +13,21 @@ const links = [
 ];
 
 const Navbar = () => {
-  const {darkMode, toggleDarkMode} = useContext(ThemeContext);
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const ToggleOpenMenu = () => {
+    setOpen((prevState) => !prevState);
+  };
+
+  const menuStyles = open ? "menu open" : "menu";
 
   return (
     <div className="navbar">
       <div className="brand">
         <span>Resume Management</span>
       </div>
-      <div className="menu">
+      <div className={menuStyles}>
         <ul>
           {links.map((items) => (
             <li key={items.href}>
@@ -30,11 +37,15 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="hamburger">
-        <Menu />
+        <Menu onClick={ToggleOpenMenu} />
       </div>
       <div className="toggle">
-        <ToggleButton value={"check"} selected={darkMode} onChange={toggleDarkMode}>
-            {darkMode ? <LightMode/> : <DarkMode/>}
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}
+        >
+          {darkMode ? <LightMode /> : <DarkMode />}
         </ToggleButton>
       </div>
     </div>

2650370510b6c85c8d8e3cfab7aa2ebf592d020d
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-04-17 09:40:22 -0700

    functional routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { useContext } from "react";
 import { ThemeContext } from "./context/theme.context";
 import Navbar from "./components/navbar/Navbar.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Companies from "./pages/companies/Companies";
+import Jobs from "./pages/jobs/Jobs";
+import Candidates from "./pages/candidates/Candidates";
 
 function App() {
   const {darkMode} = useContext(ThemeContext);
-  
+
   const appStyles = darkMode ? "app dark" : "app"
 
   return (
     <div className={appStyles}>
       <Navbar/>
       <div className="wrapper">
-        Routes
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/companies" element={<Companies/>}></Route>
+          <Route path="/jobs" element={<Jobs/>}></Route>
+          <Route path="/candidates" element={<Candidates/>}></Route>
+        </Routes>
       </div>
     </div>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="content home">
+      <h3>Welcome</h3>
+      <br />
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sint
+        iusto provident reprehenderit veritatis temporibus culpa dicta veniam
+        odit omnis dignissimos maxime aliquid sequi, quia obcaecati ducimus
+        praesentium. Quae, blanditiis doloremque ab hic quidem mollitia,
+        voluptatibus assumenda, animi maiores debitis magni numquam. Rem impedit
+        deserunt veritatis fuga iste ducimus ad minus velit reiciendis modi
+        atque ab architecto, autem ut blanditiis nemo non tenetur eligendi
+        accusantium sapiente repellendus commodi adipisci dolor iure! Aliquam
+        quidem quibusdam soluta voluptatum qui id, dolore aut quia veniam esse.
+        Est magnam deleniti dolore dolorem fuga temporibus blanditiis fugit.
+        Similique dolorum labore eaque perferendis amet laudantium harum.
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

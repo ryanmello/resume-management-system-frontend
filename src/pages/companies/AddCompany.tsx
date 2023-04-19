6e084d2f02fe079d310ea5fd576dcfff34c5c949
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-04-19 08:44:15 -0700

    create company

--- src/pages/companies/AddCompany.tsx
+++ src/pages/companies/AddCompany.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ICreateCompanyDto } from "../../types/global.typing";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import httpModule from "../../helpers/http.module";
 
 const AddCompany = () => {
-  return (
-    <div>
-      Add Company
-    </div>
-  )
-}
+  const redirect = useNavigate();
+  const [company, setCompany] = useState<ICreateCompanyDto>({
+    name: "",
+    size: "",
+  });
 
-export default AddCompany
+  const handleClickSaveBtn = () => {
+    if(company.name === "" || company.size === ""){
+      alert("Please enter a name and size");
+      return;
+    }
+
+    httpModule.post("company/create", company)
+      .then(response => redirect("/companies"))
+      .catch((error) => console.log(error));
+  }
+
+  const handleClickBackBtn = () => {
+    redirect("/companies");
+  }
+
+  return (
+    <div className="content">
+      <div className="add-company">
+        <h2>Add Company</h2>
+        <TextField
+          autoComplete="off"
+          label="Company Name"
+          variant="outlined"
+          value={company.name}
+          onChange={(e) => setCompany({ ...company, name: e.target.value })}
+        />
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Company Size</InputLabel>
+          <Select
+            value={company.size}
+            label="Company Size"
+            onChange={(e) => setCompany({ ...company, size: e.target.value })}
+          >
+            <MenuItem value="Small">Small</MenuItem>
+            <MenuItem value="Medium">Medium</MenuItem>
+            <MenuItem value="Large">Large</MenuItem>
+          </Select>
+        </FormControl>
+        <div className="btns">
+          <Button variant="outlined" color="primary" onClick={handleClickSaveBtn}>
+            Save
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClickBackBtn}>
+            Back
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddCompany;

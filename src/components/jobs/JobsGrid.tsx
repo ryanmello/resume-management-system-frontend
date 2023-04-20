f478749f3db8c794035ca643e0284649f234e086
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-04-20 09:52:10 -0700

    get jobs from database and display all jobs

---
+++ src/components/jobs/JobsGrid.tsx
@@ -0,0 +1,34 @@
+import { Box } from '@mui/material'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import React from 'react'
+import { IJob } from '../../types/global.typing'
+import moment from 'moment'
+
+const column: GridColDef[] = [
+    {field: "id", headerName: "ID", width: 100},
+    {field: "title", headerName: "Title", width: 300},
+    {field: "level", headerName: "Level", width: 200},
+    {field: "companyName", headerName: "Company Name", width: 200},
+    {field: "createdAt", headerName: "Created", width: 200,
+        renderCell: (params) => moment(params.row.createdAt).format("YYYY-MM-DD"),
+    }
+]
+
+interface IJobsGridProps {
+    data: IJob[];
+}
+
+const JobsGrid = ({ data }: IJobsGridProps) => {
+  return (
+    <Box sx={{width: "100%", height: 450}} className='jobs-grid'>
+        <DataGrid 
+            rows={data}
+            columns={column}
+            getRowId={(row) => row.id}
+            rowHeight={50}
+        />
+    </Box>
+  )
+}
+
+export default JobsGrid
--- src/pages/jobs/Jobs.tsx
+++ src/pages/jobs/Jobs.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./jobs.scss";
+import { Button, CircularProgress } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import { IJob } from "../../types/global.typing";
+import httpModule from "../../helpers/http.module";
+import JobsGrid from "../../components/jobs/JobsGrid";
 
 const Jobs = () => {
-  return (
-    <div>
-      Jobs
-    </div>
-  )
-}
+  const [jobs, setJobs] = useState<IJob[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-export default Jobs
+  useEffect(() => {
+    setLoading(true);
+    httpModule
+      .get<IJob[]>("/job/get")
+      .then((response) => {
+        setJobs(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert("Error");
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="content jobs">
+      <div className="heading">
+        <h2>Jobs</h2>
+        <Button variant="outlined">
+          <Add />
+        </Button>
+      </div>
+      {
+        loading ? <CircularProgress size={100}/> : jobs.length === 0 ? <h1>No Jobs</h1> : <JobsGrid data={jobs} />
+      }
+    </div>
+  );
+};
+
+export default Jobs;
